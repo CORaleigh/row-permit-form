6552a80ec30f2f1b9b928578649e2c3fbcebab92
justingreco <justin.greco@raleighnc.gov>
2018-06-15 11:47:25 -0400

    added more detail to email

--- src/app/mail.service.ts
+++ src/app/mail.service.ts
@@ -21,12 +21,12 @@ export class MailService {
       worktype = attributes.WORK_TYPE_OTHER;
     }
     let address = attributes.ADDRESS;
-    let message = "A right of way permit has been submitted by " + company + " requesting to perform " + worktype + " at " + address + ".  Permit can be viewed at approved here: https://ral.maps.arcgis.com/apps/webappviewer/index.html?id=2afdc9aa67934314842da1223263dfe0&query=Right_of_Way_Permit_Approval_8756%2COBJECTID%2C" + oid 
+    let message = "A right-of-way permit has been submitted by " + company + " requesting to perform " + worktype + " at " + address + ".  Permit can be viewed at approved here: https://ral.maps.arcgis.com/apps/webappviewer/index.html?id=2afdc9aa67934314842da1223263dfe0&query=Right_of_Way_Permit_Approval_8756%2COBJECTID%2C" + oid 
     let formData: FormData = new FormData();
-    formData.append('fromEmail', 'gis@raleighnc.gov');
-    formData.append('toEmail', 'gis@raleighnc.gov');
-    formData.append('message', "message");
-    formData.append('subject', 'Row permit request submitted');
+    formData.append('fromEmail', 'RightofWayServices@raleighnc.gov');
+    formData.append('toEmail', 'gis@raleighnc.gov,RightofWayServices@raleighnc.gov');
+    formData.append('message', message);
+    formData.append('subject', 'Right-of-Way Permit Request Submitted');
 
     // let params = new HttpParams()
     //   .set('f', 'json')
